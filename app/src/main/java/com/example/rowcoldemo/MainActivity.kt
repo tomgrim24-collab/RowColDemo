c56cd4e88288db9620c53aa8314c6ca277b0adbd
composedemo <tomgrim24@gmail.com>
2026-06-24 01:27:25 +0700

    Clean template code

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -6,24 +6,22 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.rowcoldemo.ui.theme.RowColDemoTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            RowColDemoTheme {
+            MaterialTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    RowColDemoScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +29,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun RowColDemoScreen(modifier: Modifier = Modifier) {
+    // Здесь будем добавлять демонстрацию Row и Column
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    RowColDemoTheme {
-        Greeting("Android")
+fun RowColDemoPreview() {
+    MaterialTheme {
+        RowColDemoScreen()
     }
 }
